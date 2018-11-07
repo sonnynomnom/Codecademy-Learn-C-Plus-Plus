40898f874ff01501f7d4a1ca922fcce6924228f4
Sonny Li <sl4142@columbia.edu>
2018-11-07 16:26:26 -0500

    Update

---
+++ 2. Variables/Dog Years/dog_years2.cpp
@@ -0,0 +1,27 @@
+// Learn C++
+// Dog Years
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  string dog_name;
+  int dog_age;
+
+  int early_years, later_years, human_years;
+
+  early_years = 21;
+
+  later_years = (dog_age - 2) * 4;
+
+  human_years = early_years + later_years;
+
+  cout << "Enter your dog's name: ";
+  cin >> dog_name;
+
+  cout << "My name is " << dog_name << "! Ruff Ruff, I am " << human_years << " years old in human years.\n";
+    
+}
