9fa5c51492b62fec653cc1c638d9b433c8b7ca1e
Sonny Li <sl4142@columbia.edu>
2018-11-27 11:59:06 -0500

    Update

---
+++ 1-hello-world/block-letters/initials.cpp
@@ -0,0 +1,18 @@
+// Fun Fact: I played guitar in a band called Attica.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  cout << "  SSS   L     \n";
+  cout << " S   S  L     \n";
+  cout << " S      L     \n";
+  cout << "  SSS   L     \n";
+  cout << "     S  L     \n";
+  cout << " S   S  L     \n";
+  cout << "  SSS   LLLLL \n";
+
+  return 0;
+
+}
