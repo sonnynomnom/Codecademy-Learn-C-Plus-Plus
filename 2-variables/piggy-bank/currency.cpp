4c92fd1ee8c8b0bc70940e93c11bca26813b97a3
Sonny Li <sl4142@columbia.edu>
2018-12-03 12:49:56 -0500

    Update currency.cpp

--- 2-variables/piggy-bank/currency.cpp
+++ 2-variables/piggy-bank/currency.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
-using namespace std;
 
 int main()
 {
 
-    double p;
-    double q;
-    double c;
+  double p;
+  double q;
+  double c;
 
-    float dollars;
+  float dollars;
 
-    cout << "🇲🇽 Enter number of Mexican Pesos: ";
-    cin >> p;
+  std::cout << "🇲🇽 Enter number of Pesos: ";
+  std::cin >> p;
 
-    cout << "🇬🇹 Enter number of Guatemalan Quetzals: ";
-    cin >> q;
+  std::cout << "🇬🇹 Enter number of Guatemalan Quetzals: ";
+  std::cin >> q;
 
-    cout << "🇸🇻 Enter number of Salvadoran Colons: ";
-    cin >> c;
+  std::cout << "🇸🇻 Enter number of Salvadoran Colons: ";
+  std::cin >> c;
 
-    dollars = 0.049 * p + 0.1305 * q + 0.1144 * c;
+  dollars = 0.049 * p + 0.1305 * q + 0.1144 * c;
 
-    cout << "Total USD = $" << dollars << "\n";
-
-    return 0;
+  std::cout << "Total USD = $" << dollars << "\n";
 
 }
