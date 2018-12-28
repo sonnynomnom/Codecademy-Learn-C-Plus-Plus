086a2cb66323948fd32558d7c10e2f8eed03b0a8
Sonny <sonnynomnom@gmail.com>
2018-12-28 08:05:38 -0500

    Update dog_years2.cpp

--- 2-variables/dog-years/dog_years2.cpp
+++ 2-variables/dog-years/dog_years2.cpp
@@ -2,25 +2,28 @@
 // Dog Years
 
 #include <iostream>
-using namespace std;
 
-int main()
-{
-
-  string dog_name;
-  int dog_age = 3;
+int main() {
+    
+  std::string dog_name;
+  int dog_age;
 
   int early_years, later_years, human_years;
 
+  std::cout << "Enter your dog's name: ";
+  std::cin >> dog_name;
+    
+  std::cout << "Enter your dog's age: ";
+  std::cin >> dog_age;
+    
   early_years = 21;
-
+  
   later_years = (dog_age - 2) * 4;
-
+    
   human_years = early_years + later_years;
 
-  cout << "Enter your dog's name: ";
-  cin >> dog_name;
-
-  cout << "My name is " << dog_name << "! Ruff Ruff, I am " << human_years << " years old in human years.\n";
-
+  std::cout << "My name is " << dog_name << "! Ruff Ruff, I am " << human_years << " years old in human years.\n";
+    
+  return 0;
+    
 }
