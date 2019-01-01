0caa2ed7c3865e63c5c41a3cdf21a0a998f6e5b4
Joanna Mathes <45324896+Die-HardProgrammer@users.noreply.github.com>
2019-01-01 15:15:43 -0600

    File containing ASCII output C++ Project
    Program proudly outputs Codecademy logo to terminal

---
+++ Codecademy-Pride.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int main ()
+{
+  //fun fact about me! Artichokes are my favorite vegetable
+  
+  std::cout << "  __________________                \n";
+  std::cout << " | |______________| |               \n";
+  std::cout << " | |              | |               \n";
+  std::cout << " | |              | |               \n";
+  std::cout << " | |              | |               \n";
+  std::cout << " | |    XXXXX     | |               \n";
+  std::cout << " | |  XX     X    | |               \n";
+  std::cout << " | |  X           | |               \n";
+  std::cout << " | |  X           | |               \n";
+  std::cout << " | |  X           | |               \n";
+  std::cout << " | |  XX     X    | |               \n";
+  std::cout << " | |    XXXXX     | |               \n";
+  std::cout << " | |              | |  ==========   \n";
+  std::cout << " | |--------------| | |          |  \n";
+  std::cout << " |__________________|  ==========   \n";
+
+}
