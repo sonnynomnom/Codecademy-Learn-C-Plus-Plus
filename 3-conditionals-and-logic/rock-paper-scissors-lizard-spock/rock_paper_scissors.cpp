8bf7578e4e5abad89854ad1d39bb968211349264
Sonny Li <sl4142@columbia.edu>
2019-01-09 14:43:44 -0500

    Update

--- 3-conditionals-and-logic/rock-paper-scissors-lizard-spock/rock_paper_scissors.cpp
+++ 3-conditionals-and-logic/rock-paper-scissors-lizard-spock/rock_paper_scissors.cpp
@@ -3,12 +3,12 @@
 
 int main() {
 
-  srand (time(NULL));
-  
+  srand(time(NULL));
+
   int computer = rand() % 3 + 1;
 
   int user;
-  
+
   std::cout << "====================\n";
   std::cout << "rock paper scissors!\n";
   std::cout << "====================\n";
@@ -16,11 +16,11 @@ int main() {
   std::cout << "1) ✊\n";
   std::cout << "2) ✋\n";
   std::cout << "3) ✌️\n";
-  
+
   std::cout << "shoot! ";
-  
+
   std::cin >> user;
-  
+
   if (user == 1)
     std::cout << "you choose: ✊\n";
   else if (user == 2)
@@ -34,65 +34,65 @@ int main() {
     std::cout << "cpu choose: ✋\n";
   else
     std::cout << "cpu choose: ✌️\n";
-  
-  
+
+
   if (user == computer) {
-    
+
     std::cout << "it's a tie!\n";
-    
-  } 
-  
+
+  }
+
   // user rock
-  
+
   else if (user == 1) {
-    
+
     if (computer == 2) {
-      
+
       std::cout << "you lost! booooo!\n";
-      
+
     }
     if (computer == 3) {
-      
+
       std::cout << "you won! woohoo!\n";
-      
+
     }
-    
+
   }
-  
+
   // user paper
-  
+
   else if (user == 2) {
-    
+
     if (computer == 1) {
-      
+
       std::cout << "you won! woohoo!\n";
-      
+
     }
     if (computer == 3) {
-      
+
       std::cout << "you lost! boo!\n";
-      
+
     }
-    
+
   }
-  
+
   // user scissors
-  
+
   else if (user == 3) {
-    
+
     if (computer == 1) {
-      
+
       std::cout << "you won! woohoo!\n";
-      
+
     }
     if (computer == 2) {
-      
+
       std::cout << "you lost! booooo!\n";
-      
+
     }
-    
+
   }
-  
+
   return 0;
-  
+
 }
